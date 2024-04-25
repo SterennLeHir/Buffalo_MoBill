42f1e3ae4e3093ee4f7580e58184e001a773880e
adenlexar <96285929+adenlexar@users.noreply.github.com>
2024-04-26 00:48:58 +0200

    Upgrade Wild Ride

--- app/src/main/java/esir/progmob/buffalo_mobill/WildRide.kt
+++ app/src/main/java/esir/progmob/buffalo_mobill/WildRide.kt
@@ -8,21 +8,37 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Vibrator
+import android.util.Log
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
 import android.widget.ImageView
 import android.widget.Toast
+import kotlin.random.Random
 
 class WildRide : ComponentActivity(), SensorEventListener  {
         private lateinit var sensorManager: SensorManager
         private lateinit var accelerometer: Sensor
         private lateinit var countDownTimer: CountDownTimer
-        private var totalPoints = 0
 
         private lateinit var rodeo: ImageView
-        private var rotRight: Boolean = true //pour savoir si on tourne à droite ou gauche
+
+        //inclinaisons
         private var selfIncline: Int = 0 //0 milieu, -1 = gauche, 1 = droite
+        private var rodIncline: Int = 0
+        private var fromRotation: Float = 0f
+        private var toRotation: Float = 0f //correspond à la valeur de rotation en degree
+        private var aller: Boolean = true
+
         private val context = this
+
+        private var shake_counter = 0
+        private val MAX_SHAKE = 15
+
+        //vibrator
+        private var vibrator: Vibrator? = null
+
+        private var dureeAnim = 2000 //durée d'un aller retour
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,45 +49,89 @@ class WildRide : ComponentActivity(), SensorEventListener  {
             accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!!
 
             rodeo = findViewById(R.id.rodeoView)
-
+            // vibrateur
+            vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
             startRotationGame()
         }
 
         private fun startRotationGame() {
-            countDownTimer = object : CountDownTimer(20000, 2000) { // 20 secondes, rotation toutes les 2 secondes
+            rodeoShake()
+            /*
+            countDownTimer = object : CountDownTimer(30000, 2000) { // 30 secondes, rotation toutes les 2 secondes
                 override fun onTick(millisUntilFinished: Long) {
-                    rotateImage()
+                    rodeoShake()
                 }
 
                 override fun onFinish() {
                     // Le jeu est terminé
-                    Toast.makeText(context,"Jeu terminé! Points totaux: $totalPoints",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context,"Jeu terminé!",Toast.LENGTH_SHORT).show()
                 }
             }
             countDownTimer.start()
+
+             */
         }
 
-        private fun rotateImage() {
-            // Rotation aléatoire vers la droite ou la gauche
-            rotRight = if (Math.random() < 0.5) true else false
-            val rotation = if (rotRight) 45f else -45f
+        private fun rodeoShake() {
+            if(shake_counter <= MAX_SHAKE){
+                rodIncline = if (Math.random() < 0.5) 1 else -1 //une chance sur 2 d'aller à droite ou gauche
+                val randomRot = 45f//Random.nextInt(15, 45).toFloat() //valeur aléatoire
+                fromRotation = 0f
+                toRotation = if (rodIncline==1) randomRot else -randomRot
 
-            // Créer une animation de rotation de 45 degrés
-            val rotateAnimation = RotateAnimation(0f, rotation, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
-            rotateAnimation.duration = 1500 // Durée de l'animation: 1,5 seconde
-            rotateAnimation.setAnimationListener(object : Animation.AnimationListener {
-                override fun onAnimationStart(animation: Animation?) {}
+                // Créer une animation de rotation
+                val rotateAnimation = RotateAnimation(fromRotation, toRotation, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+                rotateAnimation.duration = dureeAnim.toLong()/2
+                rotateAnimation.fillAfter = true // Garder l'image à sa position finale après l'animation
+                rotateAnimation.setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationStart(animation: Animation?) {}
 
-                override fun onAnimationEnd(animation: Animation?) {
-                    if ((rotRight && selfIncline == 1) || (!rotRight && selfIncline == -1)){
-                        totalPoints++
+                    override fun onAnimationEnd(animation: Animation?) {
+                        if (rodIncline != selfIncline){ //si on ne s'oriente pas bien pas assez rapidement
+                            //countDownTimer.cancel()
+                            Toast.makeText(context, "PERDUUUUUUU", Toast.LENGTH_SHORT)//ne s'affiche pas
+                            //on vibre
+                            vibrator?.vibrate(1000)
+                        }
+                        // Créer une nouvelle animation de retour
+                        rodIncline = 0
+                        fromRotation = toRotation
+                        toRotation = 0f //on va dans l'autre sens
+                        val rotateAnimation = RotateAnimation(fromRotation, toRotation, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+                        rotateAnimation.duration = dureeAnim.toLong()/2
+                        rotateAnimation.fillAfter = true // Garder l'image à sa position finale après l'animation
+                        rotateAnimation.setAnimationListener(object : Animation.AnimationListener {
+                            override fun onAnimationStart(animation: Animation?) {}
+
+                            override fun onAnimationEnd(animation: Animation?) {
+                                if (rodIncline != selfIncline){ //si on ne s'oriente pas bien pas assez rapidement
+                                    //countDownTimer.cancel()
+                                    Toast.makeText(context, "PERDUUUUUUU", Toast.LENGTH_SHORT)//ne s'affiche pas
+                                    //on vibre
+                                    vibrator?.vibrate(1000)
+                                }
+                                else{
+                                    rodeoShake()
+                                }
+                            }
+
+                            override fun onAnimationRepeat(animation: Animation?) {}
+                        })
+                        // Démarrer l'animation sur l'image
+                        rodeo.startAnimation(rotateAnimation)
                     }
-                }
 
-                override fun onAnimationRepeat(animation: Animation?) {}
-            })
-            // Démarrer l'animation sur l'image
-            rodeo.startAnimation(rotateAnimation)
+                    override fun onAnimationRepeat(animation: Animation?) {}
+                })
+
+                // Démarrer l'animation sur l'image
+                rodeo.startAnimation(rotateAnimation)
+                dureeAnim = dureeAnim - 100 //pour aller de plus en plus vite
+            }
+            else{
+                // Le jeu est terminé
+                Toast.makeText(context,"Jeu terminé!",Toast.LENGTH_SHORT).show()
+            }
         }
 
         override fun onResume() {
@@ -93,12 +153,16 @@ class WildRide : ComponentActivity(), SensorEventListener  {
             // Si incliné vers la droite
             if (inclination > 7) {
                 selfIncline = -1
-                println("Incliné vers la gauche!")
+                Log.d("","Incliné vers la gauche!")
             }
             // Si incliné vers la gauche
             else if (inclination < -7) {
                 selfIncline = 1
-                println("Incliné vers la droite!")
+                Log.d("","Incliné vers la droite!")
+            }
+            else{
+                selfIncline = 0
+                Log.d("","Pas incliné!")
             }
         }
     }
