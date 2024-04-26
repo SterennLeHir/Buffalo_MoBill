b9a99a0b03405a14fed418588d6ecf8c92c9b854
Sterenn <61694205+SterennLeHir@users.noreply.github.com>
2024-04-26 12:55:22 +0200

    MilkMaster now multiplayer

--- app/src/main/java/esir/progmob/buffalo_mobill/MilkMaster.kt
+++ app/src/main/java/esir/progmob/buffalo_mobill/MilkMaster.kt
@@ -1,9 +1,14 @@
 package esir.progmob.buffalo_mobill
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.Message
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -13,12 +18,133 @@ import androidx.activity.ComponentActivity
 class MilkMaster : ComponentActivity() {
     private lateinit var lait: View
     private var compteurLait = 0
-    private val MAX_LAIT = 30
+    private val MAX_LAIT = 10
     private var fini = false
+
+    private lateinit var alertDialog : AlertDialog
+
+    // scores pour l'affichage une fois le jeu fini
+    private var score = 0
+    private var scoreAdversaire = 0
+    var scoreSent : Boolean = false
+
+    // pour le multijoueur
+    private var isServer : Boolean = false
+    private var isMulti : Boolean = false
+    private var isReady : Boolean = false
+    private var isAdversaireReady : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.milk_master)
+        // Récupération des informations
+        isMulti = intent.getBooleanExtra("isMulti", false)
+        isServer = intent.getBooleanExtra("isServer", false)
+
+        if (!isMulti) {
+            // Affiche la boîte de dialogue lorsque l'activité est créée
+            val customAlertDialog = AlertDialogCustom(this, "BUT DU JEU", getString(R.string.RulesGame1), "JOUER") {
+                startGame()
+                alertDialog.dismiss()
+            }
+            alertDialog = customAlertDialog.create()
+            alertDialog.show()
+        } else {
+            initMulti()
+            if (!isServer) {
+                val customAlertDialog = AlertDialogCustom(this, "BUT DU JEU", getString(R.string.RulesGame1), "JOUER") {
+                    isReady = true
+                    if (isAdversaireReady) {
+                        Multiplayer.Exchange.dataExchangeClient.write("Ready")
+                        alertDialog.dismiss()
+                        startGame()
+                    }
+                }
+                alertDialog = customAlertDialog.create()
+                alertDialog.show()
+            } else {
+                val customAlertDialog =
+                    AlertDialogCustom(this, "BUT DU JEU", getString(R.string.RulesGame1), "JOUER") {
+                        isReady = true
+                        Multiplayer.Exchange.dataExchangeServer.write("Ready")
+                    }
+                alertDialog = customAlertDialog.create()
+                alertDialog.show()
+            }
+        }
+    }
+
+    private fun initMulti() {
+        if (isServer) {
+            // Initialisation du nouvel handler pour le thread d'échange de données
+            val handlerServer = object :
+                Handler(Looper.getMainLooper()) { // quand on reçoit un message on lance l'activité
+                override fun handleMessage(msg: Message) {
+                    val message = msg.obj.toString()
+                    Log.d("DATAEXCHANGE", "[ShadyShowdown Server] Message received: " + msg.obj.toString())
+                    if (msg.obj.toString().contains("Ready")) {
+                        Log.d("DATAEXCHANGE", "[ShadyShowdown] On peut lancer le jeu")
+                        alertDialog.dismiss()
+                        startGame()
+                    } else {
+                        // Quand on reçoit le score de l'adversaire on peut afficher la page de score
+                        scoreAdversaire = msg.obj.toString().toInt()
+                        if (!scoreSent) {
+                            Multiplayer.Exchange.dataExchangeServer.write(compteurLait.toString())
+                            scoreSent = true
+                        }
+                        val intent = Intent(this@MilkMaster, ScorePage::class.java)
+                        intent.putExtra("score", compteurLait)
+                        intent.putExtra("scoreAdversaire", scoreAdversaire)
+                        intent.putExtra("isMulti", true)
+                        intent.putExtra("isServer", isServer)
+                        Log.d("DATAEXCHANGE", "[MilkMaster Server] On lance la page de score")
+                        startActivityForResult(intent, 1)
+                    }
+                }
+            }
+            Log.d("DATAEXCHANGE", "Thread server relaunched")
+            Multiplayer.Exchange.dataExchangeServer.cancel()
+            Multiplayer.Exchange.dataExchangeServer = DataExchange(handlerServer)
+            Multiplayer.Exchange.dataExchangeServer.start()
+        } // on met à jour le handler
+        if (!isServer) {
+            val handlerClient = object :
+                Handler(Looper.getMainLooper()) { // quand on reçoit un message on lance l'activité
+                override fun handleMessage(msg: Message) {
+                    Log.d("DATAEXCHANGE", "[MilkMaster Client] Message received: " + msg.obj.toString())
+                    if (msg.obj.toString().contains("Ready")) {
+                        isAdversaireReady = true
+                        if (isReady) {
+                            Multiplayer.Exchange.dataExchangeClient.write("Ready")
+                            alertDialog.dismiss()
+                            startGame()
+                        }
+                    } else {
+                        // Quand on reçoit le score de l'adversaire on peut afficher la page de score
+                        scoreAdversaire = msg.obj.toString().toInt()
+                        if (!scoreSent) {
+                            Multiplayer.Exchange.dataExchangeClient.write(compteurLait.toString())
+                            scoreSent = true
+                        }
+                        val intent = Intent(this@MilkMaster, ScorePage::class.java)
+                        intent.putExtra("score", compteurLait)
+                        intent.putExtra("scoreAdversaire", scoreAdversaire)
+                        intent.putExtra("isMulti", true)
+                        intent.putExtra("isServer", isServer)
+                        Log.d("DATAEXCHANGE", "[MilkMaster Client] On lance la page de score")
+                        startActivityForResult(intent, 1)
+                    }
+                }
+            }
+            Log.d("DATAEXCHANGE", "Thread client relaunched")
+            Multiplayer.Exchange.dataExchangeClient.cancel()
+            Multiplayer.Exchange.dataExchangeClient = DataExchange(handlerClient)
+            Multiplayer.Exchange.dataExchangeClient.start()
+        } // on met à jour le handler
+    }
+
+    private fun startGame() {
         // Son de lait qui coule
         val mediaPlayer : MediaPlayer = MediaPlayer.create(this, R.raw.lait);
         val piesView: ImageView = findViewById(R.id.pies)
@@ -40,14 +166,39 @@ class MilkMaster : ComponentActivity() {
                 Toast.makeText(this, "C'est fini !", Toast.LENGTH_SHORT).show()
                 fini = true
                 seauView.setImageDrawable(resources.getDrawable(R.drawable.seau_rempli))
+                if (isMulti) {
+                    if (isServer) {
+                        Multiplayer.Exchange.dataExchangeServer.write(compteurLait.toString())
+                        scoreSent = true
+                    } else {
+                        Multiplayer.Exchange.dataExchangeClient.write(compteurLait.toString())
+                        scoreSent = true
+                    }
+                } else {
+                    Log.d("DATAEXCHANGE", "[MilkMaster Solo] On lance la page de score")
+                    val intent = Intent(this, ScorePage::class.java)
+                    intent.putExtra("score", compteurLait)
+                    intent.putExtra("isMulti", false)
+                    startActivityForResult(intent, 1)
+                }
             }
         }
     }
 
-    private fun updateLaitView() {
-        val totalHeight = (findViewById<ImageView>(R.id.seau)).height
-        val newHeight = totalHeight * compteurLait / MAX_LAIT
-        lait.layoutParams.height = newHeight
-        lait.requestLayout()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("DATAEXCHANGE", "[MilkMaster] onActivityResult, resultCode : $resultCode")
+        if (resultCode == Activity.RESULT_OK) {
+            // On récupère les scores
+            score = data?.getIntExtra("score", 0) ?: 0
+            scoreAdversaire = data?.getIntExtra("scoreAdversaire", 0) ?: 0
+            Log.d("DATAEXCHANGE", "score : $score, scoreAdversaire : $scoreAdversaire")
+            // On transmet les scores à GameList
+            val resultIntent = Intent()
+            resultIntent.putExtra("score", score)
+            resultIntent.putExtra("scoreAdversaire", scoreAdversaire)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
     }
 }
