54230d7e130584517bf65c06b5ec36e8d05265f0
Sterenn <61694205+SterennLeHir@users.noreply.github.com>
2024-04-26 11:30:49 +0200

    Create timer and add sounds Milk Master

--- app/src/main/java/esir/progmob/buffalo_mobill/MilkMaster.kt
+++ app/src/main/java/esir/progmob/buffalo_mobill/MilkMaster.kt
@@ -1,6 +1,9 @@
 package esir.progmob.buffalo_mobill
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -16,24 +19,33 @@ class MilkMaster : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.milk_master)
-
-        lait = findViewById(R.id.laitView)
-        val pieButton: Button = findViewById(R.id.pieButton)
-
-        pieButton.setOnClickListener {
-            if (compteurLait < MAX_LAIT) {
+        // Son de lait qui coule
+        val mediaPlayer : MediaPlayer = MediaPlayer.create(this, R.raw.lait);
+        val piesView: ImageView = findViewById(R.id.pies)
+        val seauView: ImageView = findViewById(R.id.seau)
+        var waiting = false
+        piesView.setOnClickListener {
+            if (mediaPlayer.isPlaying) {
+                Toast.makeText(this, "Vous allez trop vite et brusquez la vache", Toast.LENGTH_SHORT).show()
+                waiting = true
+                piesView.isClickable = false
+                Handler(Looper.getMainLooper()).postDelayed({
+                    waiting = false
+                    piesView.isClickable = true
+                }, 2000) // Délai de 2 secondes
+            } else if (compteurLait < MAX_LAIT && !waiting) { // il faut attendre la fin du son pour pouvoir cliquer à nouveau mais ne marche pas
                 compteurLait++
-                updateLaitView()
-            }
-            else if(!fini){
+                mediaPlayer.start()
+            } else if (compteurLait >= MAX_LAIT && !fini){
                 Toast.makeText(this, "C'est fini !", Toast.LENGTH_SHORT).show()
                 fini = true
+                seauView.setImageDrawable(resources.getDrawable(R.drawable.seau_rempli))
             }
         }
     }
 
     private fun updateLaitView() {
-        val totalHeight = (findViewById<ImageView>(R.id.seauView)).height
+        val totalHeight = (findViewById<ImageView>(R.id.seau)).height
         val newHeight = totalHeight * compteurLait / MAX_LAIT
         lait.layoutParams.height = newHeight
         lait.requestLayout()
