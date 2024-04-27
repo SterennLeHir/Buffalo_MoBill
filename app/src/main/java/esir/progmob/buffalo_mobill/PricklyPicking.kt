93cf0e765a6ea5f8ae76299d2edf770e265aa9e4
adenlexar <96285929+adenlexar@users.noreply.github.com>
2024-04-27 12:56:01 +0200

    Prickly Images

--- app/src/main/java/esir/progmob/buffalo_mobill/PricklyPicking.kt
+++ app/src/main/java/esir/progmob/buffalo_mobill/PricklyPicking.kt
@@ -1,51 +1,87 @@
 package esir.progmob.buffalo_mobill
 
+import android.content.Context
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
+import android.util.TypedValue
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import androidx.activity.ComponentActivity
 import kotlin.random.Random
 
 class PricklyPicking : ComponentActivity() {
 
-    // gestion des images
-    private lateinit var prickle1 : ImageView
-    private lateinit var prickle2 : ImageView
-    private lateinit var prickle3 : ImageView
-    private lateinit var prickle4 : ImageView
-    private lateinit var prickle5 : ImageView
-    private var prickles_list : MutableList<ImageView> = mutableListOf()
+    private val context = this
+    private lateinit var gest: GestureDetector
+
+    private val MAX_PRICKLES = 5
     private var screenWidth = 0
     private var screenHeight = 0
+    private lateinit var parentView : RelativeLayout
+
+    //valeurs pour extraire l'épine
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.prickly_picking)
-        // Récupération des images
-        prickle1 = findViewById(R.id.prickle1)
-        prickle2 = findViewById(R.id.prickle2)
-        prickle3 = findViewById(R.id.prickle3)
-        prickle4 = findViewById(R.id.prickle4)
-        prickle5 = findViewById(R.id.prickle5)
-        // Ajout des images dans la liste des épines
-        prickles_list.add(prickle1)
-        prickles_list.add(prickle2)
-        prickles_list.add(prickle3)
-        prickles_list.add(prickle4)
-        prickles_list.add(prickle5)
         // Initialisation de la taille de l'écran
         val metrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(metrics)
         screenWidth = metrics.widthPixels
         screenHeight = metrics.heightPixels
-        initImages(); // on initialise la position des épines
+
+        parentView = findViewById<RelativeLayout>(R.id.cowboyParentView)
+
+        //Listener gesture
+        gest = GestureDetector(this, PrickleGestureListener())
+
+        //On instancie nos épines
+        for (i in 0 until MAX_PRICKLES) {
+            createPrickle()
+        }
     }
 
-    private fun initImages() {
-        for (prickle : ImageView in prickles_list) {
-            val x = Random.nextInt(screenWidth - 50)
-            val y = Random.nextInt(screenHeight - 50)
-            prickle.x = x.toFloat()
-            prickle.y = y.toFloat()
+    private inner class PrickleGestureListener : GestureDetector.SimpleOnGestureListener() {
+        override fun onDown(e: MotionEvent): Boolean {
+            Log.d("", "onDown")
+            return true
+        }
+
+        override fun onScroll(
+            e1: MotionEvent?,
+            e2: MotionEvent,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            Log.d("", "onScroll $e1 $e2 $distanceX $distanceY")
+            return true
+        }
+    }
+
+    private fun createPrickle() {
+        //On crée notre image view epine
+        val prickle = ImageView(context)
+
+        //On set son image
+        prickle.setImageResource(R.drawable.epine)
+
+        //On set sa position
+        prickle.x = Random.nextInt(screenWidth/3, 2*screenWidth/3).toFloat()
+        prickle.y = Random.nextInt(3*screenHeight/6, 4*screenHeight/6).toFloat() //plus haut que le lasso
+        parentView.addView(prickle)
+
+        //Sa rotation
+        prickle.rotation = Random.nextInt(0, 180).toFloat()
+
+        //Son on click listener
+
+        prickle.setOnTouchListener { _, event ->
+            // Transmettre les événements tactiles au GestureDetector
+            gest.onTouchEvent(event)
         }
     }
 }
